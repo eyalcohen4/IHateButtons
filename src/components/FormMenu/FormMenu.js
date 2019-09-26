8336eed4bfb2071406bf8cbb52a5aa95cec4a832
Eyal Cohen <eyalcohen4.ec@gmail.com>
2019-09-26 13:06:48 +0300

    comments

--- src/components/FormMenu/FormMenu.js
+++ src/components/FormMenu/FormMenu.js
@@ -1,40 +1,51 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import InputLabel from '@material-ui/core/InputLabel';
-import Input from '@material-ui/core/Input';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
 const useStyles = makeStyles(theme => ({
   container: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap"
   },
   formControl: {
     margin: theme.spacing(1),
-    minWidth: 120,
-  },
+    minWidth: 120
+  }
 }));
 
-export default function DialogSelect({handleClick}) {
+export default function DialogSelect({ handleClick }) {
   const classes = useStyles();
   const [state, setState] = React.useState({
     open: false,
-    League: { name: '' },
-    Fixture: { number: '' }
+    League: { name: "" },
+    Fixture: { number: "" }
   });
 
+  /**
+   *  all comments here in this function are the same for handleChangeFixture
+   * 
+   *  const handleChangeLeague = leagueName => {
+   *    setState({ league: leagueName })
+   *  }
+   */
   const handleChangeLeague = name => event => {
+    // No need to make the event.target.value a string, he's already one
+    // No need to ...state
+    // no need to pass the name if you already know the property
     setState({ ...state, [name]: String(event.target.value) });
+    // after setting the state, call the this.props.onChange function to notify the parent something has changed
   };
 
-    const handleChangeFixture = number => event => {
+  const handleChangeFixture = number => event => {
     setState({ ...state, [number]: Number(event.target.value) });
   };
 
@@ -46,10 +57,23 @@ export default function DialogSelect({handleClick}) {
     setState({ ...state, open: false });
   };
 
+  /** give the <Select> options the value of the leagueId
+   * e.g:
+   * <Select>
+   *  {leagues.map((league) => (
+   *    <option value={league.id}>{league.name}</option>
+   *   ))}
+   * </Select>
+   */
   return (
     <div>
       <Button onClick={handleClickOpen}>Choose League</Button>
-      <Dialog disableBackdropClick disableEscapeKeyDown open={state.open} onClose={handleClose}>
+      <Dialog
+        disableBackdropClick
+        disableEscapeKeyDown
+        open={state.open}
+        onClose={handleClose}
+      >
         <DialogTitle>Fill the form</DialogTitle>
         <DialogContent>
           <form className={classes.container}>
@@ -58,8 +82,8 @@ export default function DialogSelect({handleClick}) {
               <Select
                 native
                 value={state.League}
-                onChange={handleChangeLeague ('League')}
-               // input={<Input id="age-native-simple" />}
+                onChange={handleChangeLeague("League")}
+                // input={<Input id="age-native-simple" />}
               >
                 <option value={1}>UCL</option>
                 <option value={2}>Premier League</option>
@@ -69,8 +93,8 @@ export default function DialogSelect({handleClick}) {
               <InputLabel htmlFor="age-simple">Fixture</InputLabel>
               <Select
                 value={state.age}
-                onChange={handleChangeFixture ('Fixture')}
-               // input={<Input id="age-native-simple" />}
+                onChange={handleChangeFixture("Fixture")}
+                // input={<Input id="age-native-simple" />}
               >
                 <MenuItem value={1}>1</MenuItem>
                 <MenuItem value={2}>2</MenuItem>
@@ -83,10 +107,11 @@ export default function DialogSelect({handleClick}) {
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
-          <Button 
-          onClick={handleClose} 
-          color="primary"
-          handleclick={handleClick}>
+          <Button
+            onClick={handleClose}
+            color="primary"
+            handleclick={handleClick}
+          >
             Ok
           </Button>
         </DialogActions>
--- src/containers/FootballAPI.js
+++ src/containers/FootballAPI.js
@@ -1,60 +1,76 @@
-import React, {Component} from 'react';
-import CardList from '../components/CardList/CardList';
-import Scroll from '../components/Scroll/Scroll';
-import FormMenu from '../components/FormMenu/FormMenu';
+import React, { Component } from "react";
+import CardList from "../components/CardList/CardList";
+import Scroll from "../components/Scroll/Scroll";
+import FormMenu from "../components/FormMenu/FormMenu";
 
 //const APIKEY =  '8cd109a7cd8a4fd599ae76de90536c6a';
 
 class FootballAPI extends Component {
- 	constructor() {
- 		super();
- 		this.getCompititionFixtures = this.getCompititionFixtures.bind(this);
-		this.BASE_URL = 'http://api.football-data.org/v2';
-		this.state = {
-			//group-stage: 91.
-			matches: [],
-			League: '',
-			Fixture: ''
-			}
-		}
-	
+  constructor() {
+    super();
 
-	async getCompititionFixtures() {
-		const request = 
-			await fetch('http://api.football-data.org/v2/competitions/CL/matches', {
-            	method: 'get',
-            	headers: {
-            		'Content-Type': 'application/json',
-					'X-Auth-Token':	'8cd109a7cd8a4fd599ae76de90536c6a'	
-					},
-           		})
-		const data = await request.json();
-		this.setState({
-			matches: data.matches
-			})
-		console.log(this.state)
-		}
+    // https://medium.com/quick-code/react-quick-tip-use-class-properties-and-arrow-functions-to-avoid-binding-this-to-methods-29628aca2e25
+    // can be avoided by using arrow function, see in the function defnition
+    this.getCompititionFixtures = this.getCompititionFixtures.bind(this);
+    this.BASE_URL = "http://api.football-data.org/v2";
 
-	handleClick(event) {
-		this.setState({
-			League: event.target.value,
-			Fixture: event.target.value
-		})
-	}
+    // capital case is for classes, object properties should be camalCase
+    this.state = {
+      //group-stage: 91.
+      matches: [],
+      League: "",
+      Fixture: ""
+    };
+  }
 
- 	render() {
- 		return (
-    		<div>
-    			<FormMenu handleClick={this.handleClick} />
-	    		<Scroll >
-    				<CardList matches={this.state.matches} />
-    			</Scroll>
-      		</div>
-  		);
-	}
+  // async getCompititionFixtures = () => {...}
+  async getCompititionFixtures() {
+    const request = await fetch(
+      "http://api.football-data.org/v2/competitions/CL/matches",
+      {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Auth-Token": "8cd109a7cd8a4fd599ae76de90536c6a"
+        }
+      }
+    );
+    const data = await request.json();
+    this.setState({
+      matches: data.matches
+    });
+    console.log(this.state);
+  }
+
+  //
+  async getLeagues() {
+    /**
+     * 1. request available leagues from fotball data api
+     * 2. set them in the state
+     * 3. later, pass them to the FormMenu through the props
+     */
+  }
+
+  // change the event to an object which look like:
+  // { league: <leagueId>, fixture: <number>}
+  // then set them to the state
+  handleClick(event) {
+    this.setState({
+      League: event.target.value,
+      Fixture: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <FormMenu handleClick={this.handleClick} />
+        <Scroll>
+          <CardList matches={this.state.matches} />
+        </Scroll>
+      </div>
+    );
+  }
 }
 
 export default FootballAPI;
-
-
-// 
